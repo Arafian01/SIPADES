195a712c7c8d3f404fc42d7ab7ec522628f2347c
Arafian Hafiz <arafianw@gmail.com>
2025-07-27 12:32:41 +0700

    Create 2025_07_27_043448_create_pengguna.php

---
+++ SIPADES/database/migrations/2025_07_27_043448_create_pengguna.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_perangkat');
+            $table->string('jabatan');
+            $table->string('nama_jabatan');
+            $table->string('jabatan_tim_inventarisasi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengguna');
+    }
+};
