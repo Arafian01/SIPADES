c165bd0059393f34a31fc95b33ef8384dd0ca33f
Arafian Hafiz <arafianw@gmail.com>
2025-07-27 12:32:44 +0700

    Create 2025_07_27_043545_create_ruangan.php

---
+++ SIPADES/database/migrations/2025_07_27_043545_create_ruangan.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ruangan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_pengguna')->constrained('pengguna')->onDelete('cascade');
+            $table->string('kode_ruangan');
+            $table->string('nama_ruangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ruangan');
+    }
+};
