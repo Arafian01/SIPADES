89910bdbdcad1c0c8581499b741f0b4579e23d6d
Arafian Hafiz <arafianw@gmail.com>
2025-07-31 16:53:17 +0700

    Create 2025_07_31_094823_create_aset_tetap_lainnya.php

---
+++ SIPADES/database/migrations/2025_07_31_094823_create_aset_tetap_lainnya.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aset_tetap_lainnya', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_aset')->constrained('aset')->onDelete('cascade');
+            $table->string('kode_pemilik');
+            $table->string('ruang');
+            $table->string('judul');
+            $table->string('pencipta');
+            $table->string('ukuran');
+            $table->string('bahan');
+            $table->date('tanggal_perolehan');
+            $table->string('perolehan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aset_tetap_lainnya');
+    }
+};
