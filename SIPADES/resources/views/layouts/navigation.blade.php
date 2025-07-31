fce01313d0691289ce67f950d524509b6fbe928d
Arafian Hafiz <arafianw@gmail.com>
2025-07-31 16:31:37 +0700

    Update navigation.blade.php

--- SIPADES/resources/views/layouts/navigation.blade.php
+++ SIPADES/resources/views/layouts/navigation.blade.php
@@ -73,6 +73,50 @@
                                 <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
                                     {{ __('Pengadaan Aset') }}
                                 </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Tanah') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Peralatan dan Mesin') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Gedung dan Bangunan') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Jalan, Irigasi dan Jaringan') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Aset Tetap Lainnya') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Golongan Kontruksi Dalam Pengerjaan') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </li>
+                </div>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <li class="relative list-none">
+                        <x-dropdown>
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Laporan</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"> <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('pengadaan.index')" :active="request()->routeIs('pengadaan.index')">
+                                    {{ __('Pengadaan Aset') }}
+                                </x-dropdown-link>
                             </x-slot>
                         </x-dropdown>
                     </li>
