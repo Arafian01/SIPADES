276c47b9660a3972a4f026f355c27ffad901100e
Arafian Hafiz <arafianw@gmail.com>
2025-08-04 06:35:10 +0700

    Create peralatan_dan_mesin.php

---
+++ SIPADES/app/Models/peralatan_dan_mesin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class peralatan_dan_mesin extends Model
+{
+    protected $table = 'peralatan_dan_mesin';
+    
+    protected $fillable = [
+        'id_aset',
+        'kode_pemilik',
+        'ruang',
+        'merk',
+        'ukuran',
+        'bahan',
+        'tanggal_perolehan',
+        'nomor_pabrik',
+        'nomor_rangka',
+        'nomor_mesin',
+        'nomor_polisi',
+        'nomor_bpkb',
+        'perolehan',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo('App\Models\aset', 'id_aset');
+    }
+}
