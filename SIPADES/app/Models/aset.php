efeee5c3617521b794da6bf75307d7adea3bbea7
Arafian Hafiz <arafianw@gmail.com>
2025-08-04 06:34:59 +0700

    Update aset.php

--- SIPADES/app/Models/aset.php
+++ SIPADES/app/Models/aset.php
@@ -9,11 +9,17 @@ class aset extends Model
     protected $table = 'aset';
 
     protected $fillable = [
+        'id_barang',
+        'nomor_register',
         'id_rekening',
-        'kode_aset',
-        'nama_aset',
+        'nama_label',
+        'kode_belanja_bidang',
+        'asal',
         'sumber_dana',
         'nilai_perolehan',
+        'kondisi',
+        'tanggal_pembukuan',
+        'keterangan',
     ];
 
     public function rekening()
