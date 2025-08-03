ccf844642e0bb5a141ae82c2358c41c32d7cdb56
Arafian Hafiz <arafianw@gmail.com>
2025-08-04 06:35:04 +0700

    Create gedung_dan_bangunan.php

---
+++ SIPADES/app/Models/gedung_dan_bangunan.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class gedung_dan_bangunan extends Model
+{
+    protected $table = 'gedung_dan_bangunan';
+
+    protected $fillable = [
+        'id_aset',
+        'kode_pemilik',
+        'tanggal_perolehan',
+        'luas_lantai',
+        'bertingkat',
+        'beton',
+        'no_dokumen',
+        'tanggal_dokumen',
+        'id_tanah',
+        'perolehan',
+        'alamat',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo('App\Models\aset', 'id_aset');
+    }
+
+    public function tanah()
+    {
+        return $this->belongsTo('App\Models\tanah', 'id_tanah');
+    }
+}
