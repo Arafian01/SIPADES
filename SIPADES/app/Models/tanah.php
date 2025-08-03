a0cb01a8b1e39cfdd38f5bb01113474ced96eaf9
Arafian Hafiz <arafianw@gmail.com>
2025-08-04 06:35:15 +0700

    Create tanah.php

---
+++ SIPADES/app/Models/tanah.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tanah extends Model
+{
+    protected $table = 'tanah';
+
+    protected $fillable = [
+        'id_aset',
+        'kode_pemilik',
+        'tanggal_perolehan',
+        'luas',
+        'status',
+        'tanggal_sertifikat',
+        'nomor_sertifikat',
+        'perolehan',
+        'alamat',
+        'kode_lokasi',
+        'lokasi_desa',
+        'batas_utara',
+        'batas_timur',
+        'batas_selatan',
+        'batas_barat',
+        'keterangan',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo('App\Models\aset', 'id_aset');
+    }
+}
