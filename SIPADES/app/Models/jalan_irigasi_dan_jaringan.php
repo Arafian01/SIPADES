9d4ef2610bca8a0abeee670e5bbd09462c92270a
Arafian Hafiz <arafianw@gmail.com>
2025-08-08 06:40:32 +0700

    Create jalan_irigasi_dan_jaringan.php

---
+++ SIPADES/app/Models/jalan_irigasi_dan_jaringan.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class jalan_irigasi_dan_jaringan extends Model
+{
+    protected $table = 'jalan_irigasi_dan_jaringan';
+
+    protected $fillable = [
+        'id_aset',
+        'kode_pemilik',
+        'tanggal_perolehan',
+        'kontruksi',
+        'panjang',
+        'lebar',
+        'luas',
+        'no_dokumen',
+        'tanggal_dokumen',
+        'id_tanah',
+        'perolehan',
+        'lokasi',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo('App\Models\aset', 'id_aset');
+    }
+
+    public function tanah()
+    {
+        return $this->belongsTo('App\Models\tanah', 'id_tanah');
+    }
+}
