d48ac679dbf770ab9c7527916fbf4ecfc1b1f81a
Arafian Hafiz <arafianw@gmail.com>
2025-08-08 06:40:49 +0700

    Create gaset_tetap_lainnyaController.php

---
+++ SIPADES/app/Http/Controllers/gaset_tetap_lainnyaController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\aset;
+use App\Models\aset_tetap_lainnya;
+use App\Models\rekening;
+use Illuminate\Http\Request;
+
+class gaset_tetap_lainnyaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Fetch all necessary models
+        $aset = aset::all();
+        $rekening = rekening::all();
+        $aset_tetap_lainnya = aset_tetap_lainnya::all();
+
+        // Return the view with the fetched data
+        return view('page.golongan.aset_tetap_lainnya.index', compact('aset', 'rekening', 'aset_tetap_lainnya'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // Return the view for creating a new resource
+        $rekening = rekening::all();
+        $aset = aset::all();
+        return view('page.golongan.aset_tetap_lainnya.create', compact('rekening', 'aset'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dataAset = [
+            'id_barang' => $request->input('id_barang'),
+            'nomor_register' => $request->input('no_reg'),
+            'id_rekening' => $request->input('id_rekening'),
+            'nama_label' => $request->input('nama_label'),
+            'kode_belanja_bidang' => $request->input('kode_belanja_bidang'),
+            'asal' => $request->input('asal'),
+            'sumber_dana' => $request->input('sumber_dana'),
+            'nilai_perolehan' => $request->input('nilai_perolehan'),
+            'kondisi' => $request->input('kondisi'),
+            'tanggal_pembukuan' => $request->input('tanggal_pembukuan'),
+            'keterangan' => $request->input('keterangan'),
+        ];
+        aset::create($dataAset);
+        $id_aset = aset::latest()->first()->id;
+
+        $dataAsetTetapLainnya = [
+            'id_aset' => $id_aset,
+            'kode_pemilik' => $request->input('kode_pemilik'),
+            'ruang' => $request->input('ruang'),
+            'judul' => $request->input('judul'),
+            'pencipta' => $request->input('pencipta'),
+            'ukuran' => $request->input('ukuran'),
+            'bahan' => $request->input('bahan'),
+            'tanggal_perolehan' => $request->input('tanggal_perolehan'),
+            'perolehan' => $request->input('perolehan'),
+        ];
+        aset_tetap_lainnya::create($dataAsetTetapLainnya);
+
+        return redirect()->route('aset_tetap_lainnya.index')->with('message', 'Data Aset Tetap Lainnya Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        // Fetch the specific resource by ID
+        $aset_tetap_lainnya = aset_tetap_lainnya::findOrFail($id);
+        $aset = aset::all();
+        $rekening = rekening::all();
+
+        // Return the view for editing the resource
+        return view('page.golongan.aset_tetap_lainnya.edit', compact('aset_tetap_lainnya', 'aset', 'rekening'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $aset_tetap_lainnya = aset_tetap_lainnya::findOrFail($id);
+        $aset = aset::findOrFail($aset_tetap_lainnya->id_aset);
+
+        $dataAset = [
+            'id_barang' => $request->input('id_barang'),
+            'nomor_register' => $request->input('no_reg'),
+            'id_rekening' => $request->input('id_rekening'),
+            'nama_label' => $request->input('nama_label'),
+            'kode_belanja_bidang' => $request->input('kode_belanja_bidang'),
+            'asal' => $request->input('asal'),
+            'sumber_dana' => $request->input('sumber_dana'),
+            'nilai_perolehan' => $request->input('nilai_perolehan'),
+            'kondisi' => $request->input('kondisi'),
+            'tanggal_pembukuan' => $request->input('tanggal_pembukuan'),
+            'keterangan' => $request->input('keterangan'),
+        ];
+        $aset->update($dataAset);
+        $dataAsetTetapLainnya = [
+            'kode_pemilik' => $request->input('kode_pemilik'),
+            'ruang' => $request->input('ruang'),
+            'judul' => $request->input('judul'),
+            'pencipta' => $request->input('pencipta'),
+            'ukuran' => $request->input('ukuran'),
+            'bahan' => $request->input('bahan'),
+            'tanggal_perolehan' => $request->input('tanggal_perolehan'),
+            'perolehan' => $request->input('perolehan'),
+        ];
+        $aset_tetap_lainnya->update($dataAsetTetapLainnya);
+        return redirect()->route('aset_tetap_lainnya.index')->with('message', 'Data Aset Tetap Lainnya Berhasil Diperbarui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $aset_tetap_lainnya = aset_tetap_lainnya::findOrFail($id);
+        $aset = aset::findOrFail($aset_tetap_lainnya->id_aset);
+
+        // Delete the aset_tetap_lainnya and its associated aset
+        $aset_tetap_lainnya->delete();
+        $aset->delete();
+
+        return redirect()->route('aset_tetap_lainnya.index')->with('message', 'Data Aset Tetap Lainnya Berhasil Dihapus');
+    }
+}
