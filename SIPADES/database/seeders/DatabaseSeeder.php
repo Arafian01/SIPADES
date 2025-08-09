041543ae956b9b4f673d17cb1fc6ab682dfebe66
Arafian Hafiz <arafianw@gmail.com>
2025-08-10 06:42:21 +0700

    Update DatabaseSeeder.php

--- SIPADES/database/seeders/DatabaseSeeder.php
+++ SIPADES/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\golongan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // buat 5 data golongan
+        golongan::create([
+            'nama_golongan' => 'Tanah',
+        ]);
+        golongan::create([
+            'nama_golongan' => 'Peralatan dan Mesin',
+        ]);
+        golongan::create([
+            'nama_golongan' => 'Gedung dan Bangunan',
+        ]);
+        golongan::create([
+            'nama_golongan' => 'Jalan, Irigasi, dan Jaringan',
+        ]);
+        golongan::create([
+            'nama_golongan' => 'Aset Tetap Lainnya',
+        ]);
+        golongan::create([
+            'nama_golongan' => 'Konstruksi Dalam Pengerjaan',
         ]);
     }
 }
