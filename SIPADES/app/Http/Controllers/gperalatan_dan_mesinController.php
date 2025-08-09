0e58fb9d496916ac3f6120cc3d879d83db616ca5
Arafian Hafiz <arafianw@gmail.com>
2025-08-09 11:58:07 +0700

    Update gperalatan_dan_mesinController.php

--- SIPADES/app/Http/Controllers/gperalatan_dan_mesinController.php
+++ SIPADES/app/Http/Controllers/gperalatan_dan_mesinController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\aset;
+use App\Models\detail_pengadaan;
 use App\Models\peralatan_dan_mesin;
 use App\Models\rekening;
 use App\Models\ruangan;
@@ -24,13 +25,15 @@ class gperalatan_dan_mesinController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(String $id)
     {
         $aset = aset::all();
         $rekening = rekening::all();
         $ruangan = ruangan::all();
         $peralatan_dan_mesin = peralatan_dan_mesin::all();
-        return view('page.golongan.peralatan_dan_mesin.create', compact('aset', 'rekening', 'ruangan', 'peralatan_dan_mesin'));
+
+        return view('page.golongan.peralatan_dan_mesin.create', compact('aset', 'rekening', 'ruangan', 'id'));
+        
     }
 
     /**
@@ -38,42 +41,7 @@ class gperalatan_dan_mesinController extends Controller
      */
     public function store(Request $request)
     {
-        // $dataAset = [
-        //     'id_barang' => $request->input('id_barang'),
-        //     'nomor_register' => $request->input('no_reg'),
-        //     'id_rekening' => $request->input('id_rekening'),
-        //     'nama_label' => $request->input('nama_label'),
-        //     'kode_belanja_bidang' => $request->input('kode_belanja_bidang'),
-        //     'asal' => $request->input('asal'),
-        //     'sumber_dana' => $request->input('sumber_dana'),
-        //     'nilai_perolehan' => $request->input('nilai_perolehan'),
-        //     'kondisi' => $request->input('kondisi'),
-        //     'tanggal_pembukuan' => $request->input('tanggal_pembukuan'),
-        //     'keterangan' => $request->input('keterangan'),
-        // ];
-        // aset::create($dataAset);
-        // $id_aset = aset::latest()->first()->id;
-        // $dataTanah = [
-        //     'id_aset' => $id_aset,
-        //     'kode_pemilik' => $request->input('kode_pemilik'),
-        //     'tanggal_perolehan' => $request->input('tanggal_perolehan'),
-        //     'luas' => $request->input('luas'),
-        //     'status' => $request->input('status'),
-        //     'tanggal_sertifikat' => $request->input('tanggal_sertifikat'),
-        //     'nomor_sertifikat' => $request->input('no_sertifikat'),
-        //     'perolehan' => $request->input('perolehan'),
-        //     'alamat' => $request->input('alamat'),
-        //     'kode_lokasi' => $request->input('kode_lokasi'),
-        //     'lokasi_desa' => $request->input('kode_lokasi'),
-        //     'batas_utara' => $request->input('batas_utara'),
-        //     'batas_timur' => $request->input('batas_timur'),
-        //     'batas_selatan' => $request->input('batas_selatan'),
-        //     'batas_barat' => $request->input('batas_barat'),
-        //     'keterangan' => $request->input('keterangan'),
-        // ];
-        // tanah::create($dataTanah);
-        // return back()->with('message_delete', 'Data Tanah Berhasil Ditambahkan');
-
+        $idPengadaan = $request->input('id_pengadaan');
         $dataAset = [
             'id_barang' => $request->input('id_barang'),
             'nomor_register' => $request->input('no_reg'),
@@ -106,7 +74,19 @@ class gperalatan_dan_mesinController extends Controller
             'perolehan' => $request->input('perolehan'),
         ];
         peralatan_dan_mesin::create($dataPeralatanDanMesin);
-        return back()->with('message_delete', 'Data Peralatan dan Mesin Berhasil Ditambahkan');
+
+        if ($idPengadaan > 0) {
+            $details = [
+                'id_pengadaan' => $idPengadaan,
+                'id_aset' => $id_aset,
+            ];
+            detail_pengadaan::create($details);
+            return redirect()->route('pengadaan.show', $idPengadaan)
+                ->with('message_delete', 'Data Peralatan dan Mesin Berhasil Ditambahkan');
+        } else {
+            return redirect()->route('peralatan_dan_mesin.index')
+                ->with('message_delete', 'Data Peralatan dan Mesin Berhasil Ditambahkan');
+        }
     }
 
     /**
