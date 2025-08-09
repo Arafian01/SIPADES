4841cd6b7d6421f7fb998514b96cea953cf073d0
Arafian Hafiz <arafianw@gmail.com>
2025-08-09 11:58:26 +0700

    Update pengadaan.php

--- SIPADES/app/Models/pengadaan.php
+++ SIPADES/app/Models/pengadaan.php
@@ -24,4 +24,55 @@ class pengadaan extends Model
     {
         return $this->belongsTo('App\Models\pengguna', 'id_pengguna');
     }
+
+    public function detailPengadaan()
+    {
+        return $this->hasMany('App\Models\detail_pengadaan', 'id_pengadaan');
+    }
+
+    public function aset()
+    {
+        return $this->hasMany('App\Models\aset', 'id_pengadaan');
+    }
+    public function kontruksiDalamPengerjaan()
+    {
+        return $this->hasMany('App\Models\kontruksi_dalam_pengerjaan', 'id_pengadaan');
+    }
+    public function gtanah()
+    {
+        return $this->hasMany('App\Models\gtanah', 'id_pengadaan');
+    }
+    public function gperalatanDanMesin()
+    {
+        return $this->hasMany('App\Models\gperalatan_dan_mesin', 'id_pengadaan');
+    }
+    public function ggedungDanBangunan()
+    {
+        return $this->hasMany('App\Models\ggedung_dan_bangunan', 'id_pengadaan');
+    }
+    public function gjalanIrigasiDanJaringan()
+    {
+        return $this->hasMany('App\Models\gjalan_irigasi_dan_jaringan', 'id_pengadaan');
+    }
+    public function gasetTetapLainnya()
+    {
+        return $this->hasMany('App\Models\gaset_tetap_lainnya', 'id_pengadaan');
+    }
+    public function gkontruksiDalamPengerjaan()
+    {
+        return $this->hasMany('App\Models\gkontruksi_dalam_pengerjaan', 'id_pengadaan');
+    }   
+    public function asetTetapLainnya()
+    {
+        return $this->hasMany('App\Models\aset_tetap_lainnya', 'id_pengadaan');
+    }   
+    public function rekening()
+    {
+        return $this->belongsTo('App\Models\rekening', 'id_rekening');
+    }
+    public function ruangan()
+    {
+        return $this->belongsTo('App\Models\ruangan', 'id_ruangan');
+    }
+    
 }
