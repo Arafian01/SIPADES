3657c52bfde725b1b97680233eec15b6eefbbe71
Arafian Hafiz <arafianw@gmail.com>
2025-08-12 19:15:04 +0700

    Update 2025_07_27_052237_create_tanah.php

--- SIPADES/database/migrations/2025_07_27_052237_create_tanah.php
+++ SIPADES/database/migrations/2025_07_27_052237_create_tanah.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('id_aset')->constrained('aset')->onDelete('cascade');
             $table->string('kode_pemilik');
-            $table->date('tanggal_perolehan');
             $table->string('luas');
             $table->string('status');
             $table->string('tanggal_sertifikat');
