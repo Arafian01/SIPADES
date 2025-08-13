330938240a767de03158d439cda45d4508339e8e
Arafian Hafiz <arafianw@gmail.com>
2025-08-13 14:32:57 +0700

    Update edit.blade.php

--- SIPADES/resources/views/page/golongan/gedung_dan_bangunan/edit.blade.php
+++ SIPADES/resources/views/page/golongan/gedung_dan_bangunan/edit.blade.php
@@ -122,7 +122,7 @@
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Perolehan</label>
                                 <input type="date" id="tanggal_perolehan" name="tanggal_perolehan"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    value="{{ old('tanggal_perolehan', $gedung_dan_bangunan->tanggal_perolehan) }}" required />
+                                    value="{{ old('tanggal_perolehan', $gedung_dan_bangunan->aset->tanggal_perolehan) }}" required />
                                 @error('tanggal_perolehan')
                                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                                 @enderror
