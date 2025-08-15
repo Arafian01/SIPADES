5447775394646cb27cfe502d10127e4f4c5ad9a1
Arafian Hafiz <arafianw@gmail.com>
2025-08-15 20:19:57 +0700

    Update edit.blade.php

--- SIPADES/resources/views/page/golongan/kontruksi_dalam_pengerjaan/edit.blade.php
+++ SIPADES/resources/views/page/golongan/kontruksi_dalam_pengerjaan/edit.blade.php
@@ -125,6 +125,17 @@
                                 @enderror
                             </div>
 
+                            <div class="mb-5">
+                            <label for="tanggal_perolehan"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal
+                                Perolehan</label>
+                            <input type="date" id="tanggal_perolehan" name="tanggal_perolehan"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                value="{{ old('tanggal_perolehan', $kontruksi_dalam_pengerjaan->aset->tanggal_perolehan) }}" required />
+                                @error('tanggal_perolehan')
+                                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                        </div>
 
                             <!-- Asal -->
                             <div class="mb-5">
