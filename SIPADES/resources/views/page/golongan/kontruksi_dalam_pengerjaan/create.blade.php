a60938705deb0f3de03a976ec12594118e416d73
Arafian Hafiz <arafianw@gmail.com>
2025-08-15 20:19:51 +0700

    Update create.blade.php

--- SIPADES/resources/views/page/golongan/kontruksi_dalam_pengerjaan/create.blade.php
+++ SIPADES/resources/views/page/golongan/kontruksi_dalam_pengerjaan/create.blade.php
@@ -80,7 +80,14 @@
                                 required />
                         </div>
 
-
+                        <div class="mb-5">
+                            <label for="tanggal_perolehan"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal
+                                Perolehan</label>
+                            <input type="date" id="tanggal_perolehan" name="tanggal_perolehan"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required />
+                        </div>
                         <div class="mb-5">
                             <label for="asal"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Asal</label>
