11b809c27450fe0448fe93200019a01455130268
Arafian Hafiz <arafianw@gmail.com>
2025-08-17 20:37:23 +0700

    Update laporanController.php

--- SIPADES/app/Http/Controllers/laporanController.php
+++ SIPADES/app/Http/Controllers/laporanController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\aset;
+use App\Models\aset_tetap_lainnya;
+use App\Models\gedung_dan_bangunan;
+use App\Models\jalan_irigasi_dan_jaringan;
+use App\Models\kontruksi_dalam_pengerjaan;
+use App\Models\peralatan_dan_mesin;
+use App\Models\rekening;
+use App\Models\tanah;
 use Illuminate\Http\Request;
 
 class laporanController extends Controller
@@ -60,5 +68,30 @@ class laporanController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function cetakSemua(Request $request)
+    {
+        // Ambil semua data yang dibutuhkan untuk masing-masing laporan
+        $aset = aset::all();
+        $tanah = tanah::all();
+        $rekening = rekening::all();
+        $peralatan_dan_mesin = peralatan_dan_mesin::all();
+        $kontruksi_dalam_Pengerjaan = kontruksi_dalam_pengerjaan::all();
+        $jalan_irigasi_dan_jaringan = jalan_irigasi_dan_jaringan::all();
+        $gedung_dan_bangunan = gedung_dan_bangunan::all();
+        $aset_tetap_lainnya = aset_tetap_lainnya::all();
+
+        // Kirim semua data ke 1 view
+        return view('page.report.index', compact(
+            'aset',
+            'tanah',
+            'rekening',
+            'peralatan_dan_mesin',
+            'kontruksi_dalam_pengerjaan',
+            'jalan_irigasi_dan_jaringan',
+            'gedung_dan_bangunan',
+            'aset_tetap_lainnya'
+        ));
     }
 }
